dc832e0e39fb28a6207a6a56ad5caa48ba13c371
shogohciai <shogo1104@gmail.com>
2013-12-10 01:19:40 +0900

    カテゴリ編集機能のupdateのときだけuser_idがidの値になる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
   def require_user
     @user_certification = User.find_by_id params[:user_id]  if params[:user_id].present?
     @user_certification = User.find_by_id params[:id] unless params[:user_id].present?
+    t0 = params[:user_id]
+    t1 = @user_certification
+    t2 = current_user
+    t3 = @user_certification != current_user
+    pp t0
+    pp t1
+    pp t2
+    pp t3
     redirect_to :back if current_user != @user_certification || @user_certification.nil?
   rescue ActionController::RedirectBackError
     redirect_to root_path

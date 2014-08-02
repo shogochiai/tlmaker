984d7e1b1800372acfad77e9ccd8ffab5694bae9
sgtn <shogo1104@gmail.com>
2014-08-02 18:01:23 +0900

    Update twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 #client = Twitter::REST::Client.new do |config|
-#  config.consumer_key = "9mmXkgmQMPlc7dMTbmStzg",
-#  config.consumer_secret = "74fJV4q7oF0DArCOOQg0R1TGSF9rmt8lvSH1Phd1w",
-#  config.access_token = "1026736388-cmKekQIyUwWMJoTy4cM8oEJmeirMSeKbpwoPVUv",
-#  config.access_token_secret = "NuQqQzvdytu1yJCoPRKOfU9kurM12FSOQh8Dq4USdnGRa"
+#  config.consumer_key = "",
+#  config.consumer_secret = "",
+#  config.access_token = "",
+#  config.access_token_secret = ""
 #end
